315636b89b821e5cfa6c0b3df6ae710224a2028a
Alex Berthon <Alexjberthon@gmail.com>
2023-03-24 13:06:35 -0700

    Format File Logic wrote initial function logic to read, format, and reprint strings.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -23,34 +23,34 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.fileName_TextBox = new System.Windows.Forms.TextBox();
+            this.selectFile_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // fileName_TextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 21);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(160, 23);
-            this.textBox1.TabIndex = 0;
+            this.fileName_TextBox.Location = new System.Drawing.Point(12, 21);
+            this.fileName_TextBox.Name = "fileName_TextBox";
+            this.fileName_TextBox.Size = new System.Drawing.Size(160, 23);
+            this.fileName_TextBox.TabIndex = 0;
             // 
-            // button1
+            // selectFile_button
             // 
-            this.button1.Location = new System.Drawing.Point(43, 50);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.selectFile_button.Location = new System.Drawing.Point(43, 50);
+            this.selectFile_button.Name = "selectFile_button";
+            this.selectFile_button.Size = new System.Drawing.Size(100, 23);
+            this.selectFile_button.TabIndex = 1;
+            this.selectFile_button.Text = "Select File";
+            this.selectFile_button.UseVisualStyleBackColor = true;
+            this.selectFile_button.Click += new System.EventHandler(this.FileSelectButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(184, 211);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.selectFile_button);
+            this.Controls.Add(this.fileName_TextBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -60,7 +60,7 @@
 
         #endregion
 
-        private TextBox textBox1;
-        private Button button1;
+        private TextBox fileName_TextBox;
+        private Button selectFile_button;
     }
 }
